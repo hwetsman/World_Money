431cff37d8f7b5dbfb8e6b54073ea3d880ff9b3f
hwetsman <howard@tocdr.com>
2021-12-27 15:55:17 -0600

    Chore: create dev to do list

---
+++ money.py
@@ -0,0 +1,18 @@
+
+import pandas as pd
+import os
+import requests
+
+
+"""
+To do dev list:
+Get API US Fed total assets as frequently as possible
+Get API BOE total assets as frequently as possible
+Get API ECB total assets as frequently as possible
+Get API PBC total assets as frequently as possible
+Use groupby to convert them all to the least frequent of the bunch
+Create from these a sum at the same frequency of "World_Money_Supply"
+import assets of interest(sp500, housing, bitcoin, etc)
+allow user to have start and end dates of interest
+plot the asset and the asset in total world money supply
+"""
